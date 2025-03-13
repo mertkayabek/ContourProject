a83e811e0baa2bb0da96566c2bb1bc92a4610ab8
Mert Kayabek <kayabek@student3.bauv.unibw-muenchen.de>
2025-03-13 02:35:52 +0100

    Small adjustments to new meshpy update

--- ContourProject/arc_shape_v1.py
+++ ContourProject/arc_shape_v1.py
@@ -40,27 +40,24 @@ import autograd.numpy as npAD
 import os
 
 # Import the objects we need from meshpy.
-from meshpy import (
-    mpy,	
-    Mesh,
-    MaterialReissner,
-    Beam3rHerm2Line3,
-    BoundaryCondition,
-    Rotation,
-    Function,
-    GeometrySet,
-    InputFile,
-)
-from meshpy.four_c import run_four_c
-from meshpy.utility import get_single_node
-from meshpy.utility import find_close_nodes
-from meshpy.mesh_creation_functions import (
-    create_beam_mesh_line,
+from meshpy.core.conf import mpy
+from meshpy.core.geometry_set import GeometrySet
+from meshpy.core.mesh import Mesh
+from meshpy.core.rotation import Rotation
+from meshpy.four_c.boundary_condition import BoundaryCondition
+from meshpy.four_c.element_beam import Beam3rHerm2Line3
+from meshpy.four_c.function import Function
+from meshpy.four_c.input_file import InputFile
+from meshpy.four_c.material import MaterialReissner
+from meshpy.four_c.run_four_c import run_four_c
+from meshpy.utils.nodes import get_single_node, find_close_nodes
+from meshpy.mesh_creation_functions.beam_basic_geometry import (
     create_beam_mesh_arc_segment_2d,
-    create_beam_mesh_curve,
+    create_beam_mesh_line,
 )
+from meshpy.mesh_creation_functions.beam_curve import create_beam_mesh_curve
 
-from meshpy.four_c import run_four_c
+
 def create_beams_wrapped_around_cylinder(base_dir, preview=False):
 
     interval = [0, 6] # t
@@ -252,7 +249,7 @@ def create_beams_wrapped_around_cylinder(base_dir, preview=False):
                         BoundaryCondition(
                             node_set,
                             (
-                                "NUMDOF 9 ONOFF 0 0 1 0 0 0 0 0 0 "
+                                "NUMDOF 9 ONOFF 0 0 1 1 1 1 0 0 0 "
                                 "VAL 0 0 0 0 0 0 0 0 0 "
                                 "FUNCT 0 0 0 0 0 0 0 0 0"
                             ),
@@ -456,11 +453,11 @@ if __name__ == "__main__":
     """Execution part of script."""
 
     # Adapt this path to the directory you want to store the tutorial files in.
-    output_directory = "/home_student/kayabek/sw/meshpy/ContourProject/results_stentshape4/"
+    output_directory = "/home_student/kayabek/sw/Results/arc_shape1/"
     input_file = create_beams_wrapped_around_cylinder(output_directory)
     input_file.write_input_file(os.path.join(output_directory, "simple_beam.dat"))
 
-    simulation_dir = "/home_student/kayabek/sw/meshpy/ContourProject/results_stentshape4/results"
+    simulation_dir = "/home_student/kayabek/sw/Results/arc_shape1/results"
 
     return_code = run_four_c(
         os.path.join(output_directory, "simple_beam.dat"),
--- ContourProject/arc_shape_v2.py
+++ ContourProject/arc_shape_v2.py
@@ -40,27 +40,24 @@ import autograd.numpy as npAD
 import os
 
 # Import the objects we need from meshpy.
-from meshpy import (
-    mpy,	
-    Mesh,
-    MaterialReissner,
-    Beam3rHerm2Line3,
-    BoundaryCondition,
-    Rotation,
-    Function,
-    GeometrySet,
-    InputFile,
-)
-from meshpy.four_c import run_four_c
-from meshpy.utility import get_single_node
-from meshpy.utility import find_close_nodes
-from meshpy.mesh_creation_functions import (
-    create_beam_mesh_line,
+from meshpy.core.conf import mpy
+from meshpy.core.geometry_set import GeometrySet
+from meshpy.core.mesh import Mesh
+from meshpy.core.rotation import Rotation
+from meshpy.four_c.boundary_condition import BoundaryCondition
+from meshpy.four_c.element_beam import Beam3rHerm2Line3
+from meshpy.four_c.function import Function
+from meshpy.four_c.input_file import InputFile
+from meshpy.four_c.material import MaterialReissner
+from meshpy.four_c.run_four_c import run_four_c
+from meshpy.utils.nodes import get_single_node, find_close_nodes
+from meshpy.mesh_creation_functions.beam_basic_geometry import (
     create_beam_mesh_arc_segment_2d,
-    create_beam_mesh_curve,
+    create_beam_mesh_line,
 )
+from meshpy.mesh_creation_functions.beam_curve import create_beam_mesh_curve
 
-from meshpy.four_c import run_four_c
+
 def create_beams_wrapped_around_cylinder(base_dir, preview=False):
 
     interval = [0, 6] # t
@@ -240,6 +237,7 @@ def create_beams_wrapped_around_cylinder(base_dir, preview=False):
             beam_nodes = dir1["line"].get_all_nodes()
             
             # Create a temporary submesh to apply the arc transformation
+            """
             temp_mesh = Mesh()
             for node in beam_nodes:
                 temp_mesh.nodes.append(node)
@@ -248,6 +246,7 @@ def create_beams_wrapped_around_cylinder(base_dir, preview=False):
             # The radius must be greater than half the line length
             arc_radius = R / np.sin(np.pi/2)  # Calculate required radius
             temp_mesh.transform_line_to_arc(radius=arc_radius, fixed_endpoints=True)
+            """
 
         #mesh.display_pyvista()
         #mesh.wrap_around_cylinder(radius=cylinder_radius)
@@ -483,11 +482,11 @@ if __name__ == "__main__":
     """Execution part of script."""
 
     # Adapt this path to the directory you want to store the tutorial files in.
-    output_directory = "/home_student/kayabek/sw/meshpy/ContourProject/results_stentshape5/"
+    output_directory = "/home_student/kayabek/sw/Results/arc_shape2/"
     input_file = create_beams_wrapped_around_cylinder(output_directory)
     input_file.write_input_file(os.path.join(output_directory, "simple_beam.dat"))
 
-    simulation_dir = "/home_student/kayabek/sw/meshpy/ContourProject/results_stentshape5/results"
+    simulation_dir = "/home_student/kayabek/sw/Results/arc_shape2/results"
 
     return_code = run_four_c(
         os.path.join(output_directory, "simple_beam.dat"),
